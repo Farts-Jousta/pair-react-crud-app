3fe804ff1d096aba4777fb70c347870f51ce3869
Farts-Jousta <49703765+Farts-Jousta@users.noreply.github.com>
2019-11-13 16:18:52 +1100

    added search bar

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Artists from './Artists';
+import Albums from './Albums';
+import Songs from './Songs';
 
 const Router = () => {
   return (
     <Switch>
+        <Route path="/artists" component={Artists} />
+        <Route path="/albums" component={Albums} />
+        <Route path="/songs" component={Songs} />
     </Switch>
   );
 };
